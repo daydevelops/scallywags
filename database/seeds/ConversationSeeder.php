9126693243fbce3878d862d8b7e67a248221a88b
daydevelops <adamday@daydevelops.com>
2019-11-15 18:32:13 -0330

    added extra row in conversation seeder

--- database/seeds/ConversationSeeder.php
+++ database/seeds/ConversationSeeder.php
@@ -11,26 +11,28 @@ class ConversationSeeder extends Seeder
      */
     public function run()
     {
-        // create 1 conversation for the admin and one other user
-        $convo = factory('App\Conversation')->create([
-            'channel_name' => 'testing'
-        ]);
-
-        $admin = App\User::where('is_admin',1)->first();
-        $non_admin = App\User::where('is_admin',0)->first();
-
-        $convo->users()->attach([$admin->id,$non_admin->id]);
-        
-        // 10 msgs each in alternating order
-        for ($i=0;$i<10;$i++) {
-            factory('App\Message')->create([
-                'user_id'=>$admin->id,
-                'conversation_id' => $convo->id
+        for ($i = 0; $i < 2; $i++) {
+            // create 1 conversation for the admin and one other user
+            $convo = factory('App\Conversation')->create([
+                'channel_name' => 'testing'
             ]);
-            factory('App\Message')->create([
-                'user_id'=>$non_admin->id,
-                'conversation_id' => $convo->id
-            ]);
+
+            $admin = App\User::where('is_admin', 1)->first();
+            $non_admin = App\User::where('is_admin', 0)->get()[$i];
+
+            $convo->users()->attach([$admin->id, $non_admin->id]);
+
+            // 10 msgs each in alternating order
+            for ($j = 0; $j < 10; $j++) {
+                factory('App\Message')->create([
+                    'user_id' => $admin->id,
+                    'conversation_id' => $convo->id
+                ]);
+                factory('App\Message')->create([
+                    'user_id' => $non_admin->id,
+                    'conversation_id' => $convo->id
+                ]);
+            }
         }
     }
 }
