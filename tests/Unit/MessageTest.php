c26bcfd5d51363dd4d5862d676648b3e9a29aaa6
daydevelops <adamday@daydevelops.com>
2019-11-18 19:43:46 -0330

    message views tests

--- tests/Unit/MessageTest.php
+++ tests/Unit/MessageTest.php
@@ -26,4 +26,45 @@ class MessageTest extends TestCase
 		factory('App\Message')->create();
         $this->assertInstanceOf('App\Chat',Message::first()->chat);
     }
+
+    /** @test */
+    public function it_has_views() {
+        $msg = factory('App\Message')->create();
+		DB::table('message_views')->insert([
+			'message_id'=>$msg->id,
+			'user_id'=>1
+        ]);
+        $this->assertEquals(1,$msg->views()[0]->user_id);
+    }
+
+    /** @test */
+    public function it_knows_if_it_has_been_viewed() {
+        $msg = factory('App\Message')->create();
+        $this->signIn();
+        $this->assertFalse($msg->hasViewed(auth()->id()));
+		DB::table('message_views')->insert([
+			'message_id'=>$msg->id,
+			'user_id'=>auth()->id()
+        ]);
+        $this->assertTrue($msg->hasViewed(auth()->id()));
+    }
+
+    /** @test */
+    public function a_message_can_be_viewed() {
+        $msg = factory('App\Message')->create();
+        $this->signIn();
+        $this->assertFalse($msg->hasViewed(auth()->id()));
+		$msg->viewed();
+        $this->assertTrue($msg->hasViewed(auth()->id()));
+    }
+
+    /** @test */
+    public function only_one_view_record_per_user_can_be_created() {
+        $msg = factory('App\Message')->create();
+        $this->signIn();
+		$msg->viewed();
+        $this->assertCount(1,$msg->views());
+        $msg->viewed();
+        $this->assertCount(1,$msg->views());
+    }
 }
