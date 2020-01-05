3020e5eb0faf5a904c32005549223b053a23c302
daydevelops <adamday@daydevelops.com>
2020-01-05 14:45:35 -0330

    update to laravel 6

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
-        MaddHatter\LaravelFullCalendar\ServiceProvider::class,
+        // MaddHatter\LaravelFullCalendar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -224,7 +224,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Calendar' => MaddHatter\LaravelFullCalendar\Facades\Calendar::class
+        // 'Calendar' => MaddHatter\LaravelFullCalendar\Facades\Calendar::class
     ],
 
 ];
