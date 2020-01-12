df216610f9dacb3d2008234d0f06f6644dce567c
daydevelops <adamday@daydevelops.com>
2020-01-12 16:52:31 -0330

    fixed bug due to user table column removal

--- database/migrations/2020_01_05_203039_remove_skill_from_users.php
+++ database/migrations/2020_01_05_203039_remove_skill_from_users.php
@@ -26,7 +26,7 @@ class RemoveSkillFromUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('skill');
+            $table->text('skill')->nullable();
         });
     }
 }
