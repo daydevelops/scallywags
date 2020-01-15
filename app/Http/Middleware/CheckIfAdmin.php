569c6d53bbf81bd348cc397b721556f697cf0682
daydevelops <adamday@daydevelops.com>
2020-01-15 11:17:16 -0330

    changed admin redirect login route

--- app/Http/Middleware/CheckIfAdmin.php
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -38,7 +38,7 @@ class CheckIfAdmin
         if ($request->ajax() || $request->wantsJson()) {
             return response(trans('backpack::base.unauthorized'), 401);
         } else {
-            return redirect()->guest(backpack_url('login'));
+            return redirect()->guest('login');
         }
     }
 
