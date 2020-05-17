a4481bb60bf465e90f08e8a6be4643628163cc60
daydevelops <adamday@daydevelops.com>
2020-05-17 12:18:53 -0230

    removed scheduled tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:clean')->everyMinute();
-        $schedule->command('backup:run')->everyMinute();
+        // $schedule->command('backup:clean')->everyMinute();
+        // $schedule->command('backup:run')->everyMinute();
     }
 
     /**
